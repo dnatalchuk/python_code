8677b7f4f28e1bc8e73683e68ba7683b31609ff4
Denys Natalchuk <d.natalchuk@gmail.com>
2017-03-05 14:49:40 +0100

    Codebase has been added

---
+++ Python3_code/Beat_of_python_book_examples.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Control and output hints
+
+#1
+#a = "Kola"; b = "26"; print("His name is {0} and age - {1}".format(a, b)
+
+#2
+#a=2; a *= 5; print(a)
+
+#3
+#length = 4; wideth = 5; print("Area equals {0} \nPerimetre equals {1}". format(length*wideth, 2*(length+wideth)))
+
+#4
+"""number = 10;
+guess = int(input("Enter your digit:"))
+
+if number == guess:
+    print("Hej hej!! You are correct \nBut no prise this time!")
+
+elif number < guess:
+    print("Needed digit is less than entered")
+
+else:print("Needed digit is higher than entered")
+
+print("Done")"""
+
+
