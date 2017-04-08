1c4ee37931b25f776c139801b2606a43cbc605da
Denys Natalchuk <d.natalchuk@gmail.com>
2017-04-08 21:10:54 +0200

    Code updating

--- mysql_table_data_upload.py
+++ mysql_table_data_upload.py
@@ -9,10 +9,18 @@ import pymysql
 
 #declaring variables
 db_host = 'manager.de'
+db_name = 'google'
 db_user = 'supp'
 db_pass = 'aaa'
-db_tables = 'google table'
+db_tables = 'google_date'
 
+cursor = db_name.cursor()
 #chapter for formatting data
 
 csv_data = csv.reader(sys.argv[1])
+
+def __init__(self):
+    self.conn = pymysql.connect(db_host, db_user, db_pass)
+    self.query
+
+
