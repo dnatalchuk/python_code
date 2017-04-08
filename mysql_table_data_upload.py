a2e48366ae0398803a83895d30c779e144b3914f
Denys Natalchuk <d.natalchuk@gmail.com>
2017-04-08 14:01:36 +0200

    Code updating

--- mysql_table_data_upload.py
+++ mysql_table_data_upload.py
@@ -2,3 +2,10 @@
 Scenario to upload data into MySQL specific table with file passed as argument.
 File should be formated according to table structure and perform uploading:
 """
+
+import sys
+import mysql
+
+def func():
+    pass
+
