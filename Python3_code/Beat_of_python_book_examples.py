12f33ebcb0e6e4290a2482b46063f11681d2f456
Denys Natalchuk <d.natalchuk@gmail.com>
2017-06-25 20:31:06 +0200

    Code update

--- Python3_code/Beat_of_python_book_examples.py
+++ Python3_code/Beat_of_python_book_examples.py
@@ -208,3 +208,19 @@ if (is_palindrome(something)):
 else:
     print("It is not")
 """
+
+import pickle
+
+shoplistfile = 'shoplist.data'
+shoplist = ['apples','mango','bannana']
+
+f = open(shoplistfile,'wb')
+pickle.dump(shoplist, f)
+f.close()
+
+del shoplist
+
+f = open(shoplistfile,'rb')
+storedlist = pickle.load(f)
+
+print(storedlist)
