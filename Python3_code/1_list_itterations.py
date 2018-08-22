ceb8e2fd8f0b29ac142028d4014e0c2f9f870ceb
DN <d.natalchuk@gmail.com>
2018-08-22 08:11:53 +0200

    add code

---
+++ Python3_code/1_list_itterations.py
@@ -0,0 +1,7 @@
+!#/usr/bin/python3
+for i in movies:
+    y = movies.index(i)
+    years = ['1975', '1979', '1983']
+    years_index += y
+    for stuff in movies:
+            movies.insert(years_index, stuff);
