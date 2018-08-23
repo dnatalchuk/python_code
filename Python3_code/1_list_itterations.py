ff6b3e325737875502b49d5cc901a1775dc03127
DN <d.natalchuk@gmail.com>
2018-08-23 07:02:51 +0200

    fixes

--- Python3_code/1_list_itterations.py
+++ Python3_code/1_list_itterations.py
@@ -1,7 +1,8 @@
-!#/usr/bin/python3
+#!/usr/bin/python3
+movies = ["Holy Grail","The life of brain","The meaning of life"]
 for i in movies:
     y = movies.index(i)
     years = ['1975', '1979', '1983']
-    years_index += y
-    for stuff in movies:
-            movies.insert(years_index, stuff);
+    years_index = y + 1
+    movies.insert(years_index, years[y]);
+    print(movies);
