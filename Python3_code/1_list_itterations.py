3df5139cba77c0841ea8151bdc4462074ae214fc
DN <d.natalchuk@gmail.com>
2018-08-30 23:21:00 +0200

    reffactor

--- Python3_code/1_list_itterations.py
+++ Python3_code/1_list_itterations.py
@@ -3,6 +3,6 @@ movies = ["Holy Grail","The life of brain","The meaning of life"]
 for i in movies:
     y = movies.index(i)
     years = ['1975', '1979', '1983']
-    years_index = y + 1
+    years_index = y + 1o
     movies.insert(years_index, years[y]);
     print(movies);
--- Python3_code/2_manipulating_data.py
+++ Python3_code/2_manipulating_data.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
-movies = ["Holy Grail","The life of brain",["The meaning of life", "some movie again"]]
+movies = ["Holy Grail","The life of brain",["The meaning of life", "some movie again",["sub film1", "sub film2"]]]
+
 for each_item in movies:
     if isinstance(each_item, list):
-        print(each_item);
-    else:
         for nested_item in each_item:
             if isinstance(nested_item, list):
+                for another_item in nested_item:
+                    print(another_item);
+            else:
                 print(nested_item);
+    else:
+        print(each_item)
