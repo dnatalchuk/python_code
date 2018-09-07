46f5964a372c96aed04a84d1385a6255b533d49a
DN <d.natalchuk@gmail.com>
2018-09-07 06:40:38 +0200

    create module for printer of nested lists

---
+++ Python3_code/nester/nester.py
@@ -0,0 +1,8 @@
+"""Module for displaying items in nested lists""""
+def print_nested_lists(the_list):
+"""itteract over the items in lsit, checks type of nested items, iteract and print nested items if any"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_nested_lists(each_item)
+        else:
+            print(each_item)
---
+++ Python3_code/nester/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup{
+        name = "nester",
+        version = '1.0.0',
+        py_moules = ['nester'],
+        author = 'dn',
+        author_email = 'd.natalchuk@gmail.com',
+        desciprtion = 'A simple printer of nested lists',
+}
