ce4f26df6310e1d1b1faf761142c89971c0cd96e
DN <d.natalchuk@gmail.com>
2018-09-07 06:41:51 +0200

    fix

--- Python3_code/nester/setup.py
+++ Python3_code/nester/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup{
+setup(
         name = "nester",
         version = '1.0.0',
         py_moules = ['nester'],
         author = 'dn',
         author_email = 'd.natalchuk@gmail.com',
         desciprtion = 'A simple printer of nested lists',
-}
+        )
