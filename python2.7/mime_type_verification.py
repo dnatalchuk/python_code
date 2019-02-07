a113dfeb40816df4f9d45355c0a851680d1de68d
dnatalchuk <d.natalchuk@gmail.com>
2019-02-07 14:57:27 +0100

    change data structure and fizes

--- python2.7/mime_type_verification.py
+++ python2.7/mime_type_verification.py
@@ -2,9 +2,11 @@
 
 import os
 import mimetypes
+import boto3
+import subprocess
 
 # 1. Mime-type detection for directory with assets based on file extesnions:
-dirName = ""
+dirName = os.environ['appDir']
 
 def getListOfFiles(dirName): 
     listOfFile = os.listdir(dirName)
@@ -17,17 +19,26 @@ def getListOfFiles(dirName):
             allFiles.append(fullPath)
                 
     return allFiles
-
 listOfFiles = getListOfFiles(dirName)
 
 def guess_mime():
+    file_mime_type = {}
     for file in listOfFiles:
-        mime_type = mimetypes.guess_type(file)
+        mime_type = mimetypes.guess_type(file)[0]
         if mime_type == None:
             mime_type = "text/plain"
         if mime_type == "application/octet-stream":
             raise Exception("application/octet-stream content type is not allowed")
-        return mime_type
-        print file, mime_type
+        if "healthcheck" in file:
+            mime_type = "application/json"
+        file_mime_type.update({file:mime_type})
+    return file_mime_type
+file_mime_type = guess_mime()
+print file_mime_type
 
-guess_mime()
+# # 2. Create file sets based on mime-type detected:
+cli = boto3.client('s3')
+s3 = boto3.resource('s3', region_name = "eu-west-2")
+for (key, value) in file_mime_type.iteritems():
+    s3.meta.client.upload_file(
+        key, "dn-bucket", key, ExtraArgs={'ContentType': value})
