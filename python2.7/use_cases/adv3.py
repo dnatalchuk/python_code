286c22e451557c3d3f36b74d0f98b28f763441aa
DN <d.natalchuk@gmail.com>
2019-03-18 07:41:09 +0100

    code clean-up

--- python2.7/use_cases/adv3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def f(x):
-	return x * 2
-
-if __name__ == '__main__':
-	print f(5)
--- python3/scripts/sort.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-initial_data = [1,2,3,4,5,6,11,8];
-sorted_data = sorted(initial_data, reverse=True);
-print(sorted_data)
